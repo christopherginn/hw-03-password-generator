ad0a968595da4517a83e22b5f5ee15924f012bd2
christopherginn <hey.im.chris@live.com>
2022-03-18 21:44:00 -0400

    Generator returns password

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function writePassword() {
         var lowerCaseLetters = Boolean(confirm("Lower case letters?"));
         var numbers = Boolean(confirm("Numbers?"));
         var specialCharacters = Boolean(confirm("Special characters?"));
-
         var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         var numberPool = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
         var specialCharactersPool = [];
@@ -26,22 +25,22 @@ function writePassword() {
         function generateUpperCase() {
           var upperCaseLettersIndex = Math.floor(Math.random() * letters.length);
           var upperCaseChoice = letters[upperCaseLettersIndex].toUpperCase();
+          // console.log(upperCaseChoice);
           return upperCaseChoice;
-          console.log(upperCaseChoice);
         };
   
         function generateLowerCase() {
           var lowerCaseIndex = Math.floor(Math.random() * letters.length);
           var lowerCaseChoice = letters[lowerCaseIndex].toLowerCase();
+          // console.log(lowerCaseChoice);
           return lowerCaseChoice;
-          console.log(lowerCaseChoice);
         };
   
         function generateNumbers() {
           var numbersIndex = Math.floor(Math.random() * numberPool.length);
           var numbersChoice = numberPool[numbersIndex];
+          // console.log(numbersChoice);
           return numbersChoice;
-          console.log(numbersChoice);
         };
   
         function generateSpecial() {
@@ -51,11 +50,25 @@ function writePassword() {
 
         if (!upperCaseLetters && !lowerCaseLetters && !numbers && !specialCharacters) {
           alert("No criteria established.")
-        } else if (!specialCharacters) {
-          var availableFunctions = [generateUpperCase(), generateLowerCase(), generateNumbers()];
-          var availableIndex = Math.floor(Math.random() * availableFunctions.lnegth);
-          
+        } else if (upperCaseLetters == true && lowerCaseLetters == true && numbers == true && !specialCharacters) {
+          function randomFunction() {
+            var availableFunctions = [generateUpperCase(), generateLowerCase(), generateNumbers()];
+              function availableIndex() {
+                var index = Math.floor(Math.random() * availableFunctions.length);
+                return index;
+              }
+            return availableFunctions[availableIndex()];
+          };
 
+          var passwordArray = [];
+
+          for (var i = 0; i < passwordLength; i++) {
+            passwordArray.push(randomFunction());
+          }
+          console.log(passwordArray);
+          passwordArrayText = passwordArray.join("");
+          alert("Your password is ready! Click OK to reveal it!");
+          return passwordArrayText;
 
         } else {
           alert("This isn't working");
