c21771eacad661acaf0d4390d75b817b71b9c85e
christopherginn <hey.im.chris@live.com>
2022-03-18 20:50:17 -0400

    Trying to add functions

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function writePassword() {
   passwordText.value = password;
   
     function generatePassword () {
+
+
       var passwordLength = Number(prompt("How many characters would you like your password to have?" + "Passwords must be 8-128 characters."));
       if (passwordLength >= 8 && passwordLength <= 128) {
         var upperCaseLetters = Boolean(confirm("Upper case letters?"));
@@ -17,15 +19,58 @@ function writePassword() {
         var numbers = Boolean(confirm("Numbers?"));
         var specialCharacters = Boolean(confirm("Special characters?"));
 
-        // Log to console to ensure entries are accurate
-        console.log("Password length:", passwordLength)
-        console.log("Upper Case letters:", upperCaseLetters);
-        console.log("Lower case letters:", lowerCaseLetters);
-        console.log("Numbers:", numbers);
-        console.log("Special Characters", specialCharacters);
+        var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        var numberPool = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+        var specialCharactersPool = [];
+  
+        function generateUpperCase() {
+          var upperCaseLettersIndex = Math.floor(Math.random() * letters.length);
+          var upperCaseChoice = letters[upperCaseLettersIndex].toUpperCase();
+          return upperCaseChoice;
+          console.log(upperCaseChoice);
+        };
+  
+        function generateLowerCase() {
+          var lowerCaseIndex = Math.floor(Math.random() * letters.length);
+          var lowerCaseChoice = letters[lowerCaseIndex].toLowerCase();
+          return lowerCaseChoice;
+          console.log(lowerCaseChoice);
+        };
+  
+        function generateNumbers() {
+          var numbersIndex = Math.floor(Math.random() * numberPool.length);
+          var numbersChoice = numberPool[numbersIndex];
+          return numbersChoice;
+          console.log(numbersChoice);
+        };
+  
+        function generateSpecial() {
+          var specialIndex = Math.floor(Math.random() * specialCharactersPool.length);
+          var specialChoice = specialCharactersPool[specialIndex];
+        };
+
+        if (!upperCaseLetters && !lowerCaseLetters && !numbers && !specialCharacters) {
+          alert("No criteria established.")
+        } else if (!specialCharacters) {
+          var availableFunctions = [generateUpperCase(), generateLowerCase(), generateNumbers()];
+          var availableIndex = Math.floor(Math.random() * availableFunctions.lnegth);
+          
+
+
+        } else {
+          alert("This isn't working");
+        }
+
       } else {
-        alert("Sorry, your entered an invalid option.");
-      }
+        alert("Sorry, you've entered an invalid option.");
+      };
+      // Log to console to ensure entries are accurate
+      // console.log("Password length:", passwordLength)
+      // console.log("Upper Case letters:", upperCaseLetters);
+      // console.log("Lower case letters:", lowerCaseLetters);
+      // console.log("Numbers:", numbers);
+      // console.log("Special Characters", specialCharacters);
+      // console.log();
     }
     
 }
