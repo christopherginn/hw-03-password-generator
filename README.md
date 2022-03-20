ebf73198e7fba3edbb17e546145d43b3beca2ed3
christopherginn <hey.im.chris@live.com>
2022-03-20 12:36:18 -0400

    consolidated code to reduce redundancy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hw-03-password-generator
+
+https://bobbyhadz.com/blog/javascript-convert-array-to-string-without-commas#:~:text=To%20convert%20an%20array%20to,joined%20by%20the%20provided%20separator.&text=Copied!
--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function writePassword() {
   
     function generatePassword () {
 
+      // Prompts user for input. First, the password length. 
+      // If a valid length is entered, then the user receives a series of prompts to establish criteria.
 
       var passwordLength = Number(prompt("How many characters would you like your password to have?" + "Passwords must be 8-128 characters."));
       if (passwordLength >= 8 && passwordLength <= 128) {
@@ -18,10 +20,34 @@ function writePassword() {
         var lowerCaseLetters = Boolean(confirm("Lower case letters?"));
         var numbers = Boolean(confirm("Numbers?"));
         var specialCharacters = Boolean(confirm("Special characters?"));
+
         var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         var numberPool = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
         var specialCharactersPool = ["!", "@", "#", "$", "%", "&", "*", "_"];
+
+        // Builds the criteria for the password based on user's answers to the prompts.
+        // stores functions for random characters in their own array. availableFunctions 
+
+        var availableFunctions = [];
+
+        if (upperCaseLetters) {
+          availableFunctions.push(generateUpperCase);
+        }
+
+        if (lowerCaseLetters) {
+          availableFunctions.push(generateLowerCase);
+        }
+
+        if (numbers) {
+          availableFunctions.push(generateNumbers);
+        }
+
+        if (specialCharacters) {
+          availableFunctions.push(generateSpecial);
+        }
   
+        // functions to generate random characters based on their respective arrays
+
         function generateUpperCase() {
           var upperCaseLettersIndex = Math.floor(Math.random() * letters.length);
           var upperCaseChoice = letters[upperCaseLettersIndex].toUpperCase();
@@ -49,17 +75,19 @@ function writePassword() {
           return specialChoice;
         };
 
-        if (!upperCaseLetters && !lowerCaseLetters && !numbers && !specialCharacters) {
-          alert("No criteria established.")
-        } else if (upperCaseLetters == true && lowerCaseLetters == true && numbers == true && specialCharacters == true) {
-          function randomFunction() {
-            var availableFunctions = [generateUpperCase(), generateLowerCase(), generateNumbers(), generateSpecial()];
-              function availableIndex() {
-                var index = Math.floor(Math.random() * availableFunctions.length);
-                return index;
-              }
-            return availableFunctions[availableIndex()];
-          };
+        // function to randomize placement of characters in password
+
+        function randomFunction() {
+          function availableIndex() {
+          var index = Math.floor(Math.random() * availableFunctions.length);
+          return index;
+          }
+          return availableFunctions[availableIndex()](); // () at the end will call the functions stored in availableFunctions array
+        }
+        
+        
+
+        if (availableFunctions.length > 0) {
 
           var passwordArray = [];
 
@@ -67,16 +95,20 @@ function writePassword() {
             passwordArray.push(randomFunction());
           }
           console.log(passwordArray);
-          passwordArrayText = passwordArray.join("");
+          passwordArrayText = passwordArray.join(""); // .join displays array without any commas
           alert("Your password is ready! Click OK to reveal it!");
           return passwordArrayText;
-
+          
         } else {
-          alert("This isn't working");
+          alert("No criteria determined. Please try again.");
+          var notWorkingString = "Password could not be generated.";
+          return notWorkingString;
         }
 
       } else {
         alert("Sorry, you've entered an invalid option.");
+        var notWorkingString = "Password could not be generated.";
+        return notWorkingString;
       };
       // Log to console to ensure entries are accurate
       // console.log("Password length:", passwordLength)
